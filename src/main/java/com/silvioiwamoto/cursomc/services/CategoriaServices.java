2c462f73b3e429df79787281646e3df4b52084eb
Silvio Iwamoto <silvioiwamoto@gmail.com>
2020-01-01 23:24:48 -0300

    Ajustes mp endpoint /categorias/{id}

--- src/main/java/com/silvioiwamoto/cursomc/services/CategoriaServices.java
+++ src/main/java/com/silvioiwamoto/cursomc/services/CategoriaServices.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.silvioiwamoto.cursomc.domain.Categoria;
 import com.silvioiwamoto.cursomc.repositories.CategoriaRepository;
+import com.silvioiwamoto.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaServices {
@@ -16,6 +17,7 @@ public class CategoriaServices {
 		
 		public Categoria buscar(Integer id) {
 			Optional<Categoria> obj = repo.findById(id);
-			return obj.orElse(null);
+			return obj.orElseThrow(() -> new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 		}		
 }
