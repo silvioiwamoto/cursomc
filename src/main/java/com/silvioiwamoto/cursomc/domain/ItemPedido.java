0b2ebaed5f0b7df6984e650feb7b91e1a3a9f98e
Silvio Iwamoto <silvioiwamoto@gmail.com>
2020-01-03 00:33:38 -0300

    Endpoint /pedido/{id} disponibilizado

--- src/main/java/com/silvioiwamoto/cursomc/domain/ItemPedido.java
+++ src/main/java/com/silvioiwamoto/cursomc/domain/ItemPedido.java
@@ -5,10 +5,13 @@ import java.io.Serializable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
 	
@@ -18,7 +21,7 @@ public class ItemPedido implements Serializable {
 	
 	
 	public ItemPedido() {
-			}
+	}
 
 
 	public ItemPedido(Pedido pedido, Produto produto, Double desconto, Integer quantidade, Double preco) {
@@ -29,7 +32,8 @@ public class ItemPedido implements Serializable {
 		this.quantidade = quantidade;
 		this.preco = preco;
 	}
-
+	
+	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
